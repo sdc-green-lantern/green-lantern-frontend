9aaab5e5a6ae44d2c5339330f6f6ef0cb0419cef
Qingzhou Yan <qingzhouyan@gmail.com>
2022-08-24 12:16:33 -0700

    basic set up of the project, webpack, .gitignore, client/src/components folder

---
+++ client/src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Comparison from '../Comparison/Comparison.jsx'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Comparison />
+      </div>
+    )
+  }
+};
+
+export default App;
