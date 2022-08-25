86659ac754cfca908bd325cbbd7b183b0e327c1c
Thomas Hepner <hepner.thomas@gmail.com>
2022-08-25 15:00:00 -0700

    fix: run eslint on entire project directory.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom/client'
-import App from './components/App/App.jsx'
+import * as ReactDOM from 'react-dom/client';
+import App from './components/App/App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
+root.render(<App />);
