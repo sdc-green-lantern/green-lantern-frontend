363e3af23e5b885d2adce5685a856629ced6e27c
allanviguilla <viguilla.allan@gmail.com>
2022-08-26 10:27:09 -0700

    layout changes add arrows

--- client/src/components/ProductOverview/Carousel/Carousel.jsx
+++ client/src/components/ProductOverview/Carousel/Carousel.jsx
@@ -5,7 +5,12 @@ export default function Carousel() {
   return (
     <div className={carouselstyles.imagegallery}>
       <div className={carouselstyles.thumbnailrow}>
+        {/* Map over all images pulled from API */}
         thumbnails
+      </div>
+      <div className={carouselstyles.arrows}>
+        <div className={carouselstyles.leftarrow}>❮</div>
+        <div className={carouselstyles.rightarrow}>❯</div>
       </div>
     </div>
   );
