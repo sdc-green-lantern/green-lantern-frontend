99865492c1e44bf1ea6da1a5ef47a0a5ae9614ff
allanviguilla <viguilla.allan@gmail.com>
2022-08-26 08:52:14 -0700

    clean up code with linter

--- client/src/components/ProductOverview/Carousel/Carousel.jsx
+++ client/src/components/ProductOverview/Carousel/Carousel.jsx
@@ -1,26 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import carouselstyles from './Carousel.module.css';
 
 export default function Carousel() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const sampleSlides = [
-    {url: 'https://via.placeholder.com/475x500', title: 'placeholder'},
-    {url: 'https://via.placeholder.com/475x500', title: 'placeholder'},
-    {url: 'https://via.placeholder.com/475x500', title: 'placeholder'},
-    {url: 'https://via.placeholder.com/475x500', title: 'placeholder'},
-    {url: 'https://via.placeholder.com/475x500', title: 'placeholder'}
-  ];
-
-  const slideStyles = {
-    backgroundImage: `url(${slides[currentIndex].url})`
-  }
-
   return (
-    <div
-    className={carouselstyles.imagegallery}
-    slides={sampleSlides}
-    >
-    </div>
+    <div className={carouselstyles.imagegallery} />
   );
 }
