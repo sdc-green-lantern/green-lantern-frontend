683951c4541f7b32165aeee95b1cf1461f4c26b0
allanviguilla <viguilla.allan@gmail.com>
2022-08-26 09:30:05 -0700

    add section for thumbnails

--- client/src/components/ProductOverview/Carousel/Carousel.jsx
+++ client/src/components/ProductOverview/Carousel/Carousel.jsx
@@ -3,6 +3,10 @@ import carouselstyles from './Carousel.module.css';
 
 export default function Carousel() {
   return (
-    <div className={carouselstyles.imagegallery} />
+    <div className={carouselstyles.imagegallery}>
+      <div className={carouselstyles.thumbnailrow}>
+        thumbnails
+      </div>
+    </div>
   );
 }
