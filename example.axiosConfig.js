0ffda619ce71e5a3f3ac75ba95c94be87d90c354
amberlyM <amberm1205@gmail.com>
2022-08-27 16:10:46 -0700

    added example.axiosConfig and import in App.jsx

---
+++ example.axiosConfig.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/rfp',
+});
+
+instance.defaults.headers.common['Authorization'] = '**YOUR_AUTH_TOKEN**';
+
+export default instance;
