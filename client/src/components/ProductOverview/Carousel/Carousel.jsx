a4dde8a3f1c94bd19fe4562446243b24ce0117c0
allanviguilla <viguilla.allan@gmail.com>
2022-08-30 12:23:09 -0700

    get API data into component

--- client/src/components/ProductOverview/Carousel/Carousel.jsx
+++ client/src/components/ProductOverview/Carousel/Carousel.jsx
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import axiosConfig from '../../../../../axiosConfig.js';
 import carouselstyles from './Carousel.module.css';
 
-export default function Carousel(props) {
-  const [productStyles, setProductStyles] = useState('');
-
-  const { productId, axiosConfig } = props;
+export default function Carousel() {
+  const [productImages, setProductImages] = useState([]);
 
   useEffect(() => {
-    axiosConfig.get(`/products/${productId}/styles`)
+    axiosConfig.get('/products/65633/styles')
       .then((response) => {
-
+        const result = response.data.results;
+        console.log(result);
+        setProductImages(result);
+        console.log('products... ', products);
       })
-  });
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
   const imageGallery = {
     width: '100%',
@@ -22,20 +27,21 @@ export default function Carousel(props) {
     backgroundImage: 'url(https://via.placeholder.com/400x600)',
   };
 
+  const thumbnails = {
+    cursor: 'pointer',
+    border: '1px solid red',
+    height: '50px',
+    width: '50px',
+    margin: '25px auto',
+    // backgroundImage: `${productImage.photos.thumbnail_url}`,
+  };
+
   return (
     <div className={carouselstyles.imageGallery} style={imageGallery}>
-      {/* <img className={carouselstyles.currentImage} src="https://via.placeholder.com/400x600" /> */}
       <div className={carouselstyles.thumbnailRow}>
-        {/* Map over all images pulled from API */}
-        <div className={carouselstyles.thumbnail} />
-        <div className={carouselstyles.thumbnail} />
-        <div className={carouselstyles.thumbnail} />
-        <div className={carouselstyles.thumbnail} />
-        <div className={carouselstyles.thumbnail} />
-        <div className={carouselstyles.thumbnail} />
-        <div className={carouselstyles.thumbnail} />
-        <div className={carouselstyles.thumbnail} />
-        <div className={carouselstyles.thumbnail} />
+        {productImages.map((productImage) => (
+          <div url={productImage.photos} style={thumbnails} />
+        ))}
         <div className={carouselstyles.goToNext}>▼</div>
       </div>
       <div className={carouselstyles.arrows}>
