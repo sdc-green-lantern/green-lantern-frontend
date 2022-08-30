248828167170a8216b46826e9cf28ec56c34b4e8
Thomas Hepner <hepner.thomas@gmail.com>
2022-08-30 10:55:22 -0700

    feat: individual review tile broken into areas with flexbox.

--- client/src/components/RatingsReviews/ReviewTile.jsx
+++ client/src/components/RatingsReviews/ReviewTile.jsx
@@ -11,22 +11,28 @@ class ReviewTile extends React.Component {
     const { review } = this.props;
     return (
       <div className={RatingsReviewsCSS.review_tile}>
-        <div>
-          {review.rating}
-          {review.date}
+        <div className={RatingsReviewsCSS.review_header}>
+          <div className={RatingsReviewsCSS.stars}>
+            ★★★★★
+          </div>
+          <div className={RatingsReviewsCSS.review_date}>
+            {review.date}
+          </div>
         </div>
-        <div>{review.summary}</div>
-        <div>{review.body}</div>
+        <div className={RatingsReviewsCSS.review_summary}>
+          {review.summary}
+        </div>
+        <div className={RatingsReviewsCSS.review_body}>
+          {review.body}
+        </div>
         <div>{review.recommend}</div>
         <div>{review.reviewer_name}</div>
         <div>Response to Review</div>
-        <div>
+        <div className="review_actions">
           Helpful?
           Yes
-          {/* <button>Yes</button> */}
           {review.helpfulness}
           | Report
-          {/* <button>Report</button> */}
         </div>
 
       </div>
