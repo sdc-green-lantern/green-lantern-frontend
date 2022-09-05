d9c25b72fe8580a056e02bf71a17ff8915d43fb0
Thomas Hepner <hepner.thomas@gmail.com>
2022-09-04 17:48:00 -0700

    feat: add rating overlay for stars in ratings breakdown.

--- client/src/components/RatingsReviews/Breakdowns/RatingsBreakdown.jsx
+++ client/src/components/RatingsReviews/Breakdowns/RatingsBreakdown.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faRegStar, faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+
 import RatingsBreakdownCSS from './RatingsBreakdown.module.css';
 
 export default class RatingsBreakdown extends React.Component {
@@ -13,23 +17,44 @@ export default class RatingsBreakdown extends React.Component {
       ratingProportions, ratingCounts,
     } = this.props;
 
-    // const ratingsBar = ratingProportions.m
+    const ratings = Object.keys(ratingProportions).reverse();
+    const proportions = Object.values(ratingProportions).reverse();
+    const ratingsBar = ratings.map((rating, index) => (
+      <div key={rating} className={RatingsBreakdownCSS.proportions_box}>
+        {`${rating} Stars`}
+        {/* {proportions[index]} */}
+        <div className={RatingsBreakdownCSS.grey_bar} />
+      </div>
+    ));
 
     return (
-      <div>
+      <div className={RatingsBreakdownCSS.breakdown_container}>
         <div className={RatingsBreakdownCSS.top_box}>
           <div>
             <h1>{avgRating}</h1>
           </div>
-          <div>
-            ★★★★★
+          <div className={RatingsBreakdownCSS.rating}>
+            <div
+              className={RatingsBreakdownCSS.rating_overlay}
+              style={{ width: `${avgRating * 20}%` }}
+            />
+            {/* <div className={RatingsBreakdownCSS.star}>☆</div>
+              <div className={RatingsBreakdownCSS.star}>☆</div>
+              <div className={RatingsBreakdownCSS.star}>☆</div>
+              <div className={RatingsBreakdownCSS.star}>☆</div>
+              <div className={RatingsBreakdownCSS.star}>☆</div> */}
+            <FontAwesomeIcon icon={faStar} size="lg" className={RatingsBreakdownCSS.star} />
+            <FontAwesomeIcon icon={faStar} size="lg" className={RatingsBreakdownCSS.star} />
+            <FontAwesomeIcon icon={faStar} size="lg" className={RatingsBreakdownCSS.star} />
+            <FontAwesomeIcon icon={faStar} size="lg" className={RatingsBreakdownCSS.star} />
+            <FontAwesomeIcon icon={faStar} size="lg" className={RatingsBreakdownCSS.star} />
           </div>
         </div>
         <div className={RatingsBreakdownCSS.recommmend_box}>
           <p>{`${pctRecommend} of reviews recommend this product.`}</p>
         </div>
-        <div className={RatingsBreakdownCSS.proportions_box}>
-          <p>{`Rating Proportions: ${ratingProportions}`}</p>
+        <div>
+          {ratingsBar}
         </div>
       </div>
     );
