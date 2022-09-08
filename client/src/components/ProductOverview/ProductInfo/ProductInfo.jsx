a36dfef7045174281d4c68942348eb920eb1c423
amberlyM <amberm1205@gmail.com>
2022-09-07 20:24:42 -0700

    added styling

--- client/src/components/ProductOverview/ProductInfo/ProductInfo.jsx
+++ client/src/components/ProductOverview/ProductInfo/ProductInfo.jsx
@@ -46,13 +46,7 @@ export default function ProductInfo(props) {
         setShowModal={setShowModal}
       />
       <div className={piStyles.productoptions}>
-<<<<<<< HEAD
-        <div className={piStyles.starRatings}>
-          <button type="submit" className={piStyles.goToReviews} onClick={() => window.scrollTo({ top: 1900, left: 0, behavior: 'smooth' })}>Read all reviews</button>
-        </div>
-=======
         <div className={piStyles.starRatings}><Stars />&nbsp;<a href="#read-reviews" className={piStyles.reviews}>Read all reviews</a></div>
->>>>>>> master
         <div className={piStyles.productTitle}>
           <h3>{product.category}</h3>
           <h1>{product.name}</h1>
@@ -65,22 +59,12 @@ export default function ProductInfo(props) {
           currentStyles={currentStyles}
           setCurrentStyles={setCurrentStyles}
         />
-<<<<<<< HEAD
-        <Buttons
-          // styles={styles}
-          // stylesIndex={stylesIndex}
-          // setStyleIndex={setStylesIndex}
-          currentStyles={currentStyles}
-          // key={currentStyles.style_id}
-        />
-=======
         <Buttons currentStyles={currentStyles} />
         <div className={piStyles.social}>
           <a href="https://www.facebook.com/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/128/4494/4494475.png" /></a>
           <a href="https://twitter.com/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/128/4494/4494477.png" /></a>
           <a href="https://www.pinterest.com/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/128/145/145808.png" /></a>
         </div>
->>>>>>> master
       </div>
     </div>
   );
