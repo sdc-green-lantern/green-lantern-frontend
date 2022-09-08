7aba712fe7d7a36f34daa7d318e13bdc06454cb3
Qingzhou Yan <qingzhouyan@gmail.com>
2022-09-08 00:06:14 -0700

    add 404 not found

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ const path = require('path');
 app.use(cors());
 app.use(express.json());
 
-app.use('/products/:id', express.static(path.join(__dirname, '../client/dist/')));
+app.use('/products', express.static(path.join(__dirname, '../client/dist/')));
 
-app.use('/', (req, res) => {
-  res.status('404').send('<h1>......404 Not Found</h1>');
+app.get('*', (req, res) => {
+  res.status('404').send('<h1 style="text-align:center; margin: 200px">......404 Not Found</h1>');
 });
 
 const PORT = process.env.PORT || 3030;
